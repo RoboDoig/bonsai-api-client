cd539fea434648740b2d1f6a7f10e8cfee3647fa
RoboDoig <erskine94@gmail.com>
2022-11-25 13:12:01 +0000

    proto

---
+++ bonsai-api-client/Services/IWorkflowEditor.cs
@@ -0,0 +1,18 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonsai_api_client.Services
+{
+    internal interface IWorkflowEditor
+    {
+        public ExpressionBuilderGraph Workflow { get; protected set; }
+
+        public void AddNode();
+
+        public void DeleteNode();
+    }
+}
---
+++ bonsai-api-client/Services/WorkflowEditor.cs
@@ -0,0 +1,29 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonsai_api_client.Services
+{
+    public class WorkflowEditor : IWorkflowEditor
+    {
+        private ExpressionBuilderGraph workflow;
+        ExpressionBuilderGraph IWorkflowEditor.Workflow
+        {
+            get { return workflow; }
+            set { workflow = value; }
+        }
+
+        public void AddNode()
+        {
+            System.Diagnostics.Debug.WriteLine("AddNode");
+        }
+
+        public void DeleteNode()
+        {
+            System.Diagnostics.Debug.WriteLine("DeleteNode");
+        }
+    }
+}
