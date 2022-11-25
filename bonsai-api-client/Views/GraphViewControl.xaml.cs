9f19ee1c58c95a565ff0d2e7b2f7b582e0d0d2c9
RoboDoig <erskine94@gmail.com>
2022-11-25 16:13:17 +0000

    Basic canvas setup

--- bonsai-api-client/Views/GraphViewControl.xaml.cs
+++ bonsai-api-client/Views/GraphViewControl.xaml.cs
@@ -5,15 +5,31 @@ namespace bonsai_api_client.Views;
 public partial class GraphViewControl : ContentView, IGraphView
 {
     WorkflowEditor WorkflowEditor;
+	public GraphViewControlCanvas GraphViewControlCanvas { get; set; }
 
     public GraphViewControl()
 	{
 		InitializeComponent();
 
 		WorkflowEditor = new WorkflowEditor(ServiceProvider.Current, this);
+		GraphViewControlCanvas = new GraphViewControlCanvas();
+
+		BindingContext = this;
 	}
 
 	IEnumerable<GraphNodeGrouping> IGraphView.Nodes => throw new NotImplementedException();
 
 	IEnumerable<GraphNode> IGraphView.SelectedNodes => throw new NotImplementedException();
+
+
+}
+
+public class GraphViewControlCanvas : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.StrokeColor = Colors.Red;
+        canvas.StrokeSize = 6;
+        canvas.DrawLine(10, 10, 90, 100);
+    }
 }
