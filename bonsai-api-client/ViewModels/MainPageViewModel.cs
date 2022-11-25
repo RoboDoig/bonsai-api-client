3164aea286a27926c53e191dc1483037ff293d6d
RoboDoig <erskine94@gmail.com>
2022-11-25 11:18:55 +0000

    Test button

--- bonsai-api-client/ViewModels/MainPageViewModel.cs
+++ bonsai-api-client/ViewModels/MainPageViewModel.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bonsai.Editor.GraphModel;
 
 namespace bonsai_api_client.ViewModels
 {
     public class MainPageViewModel
     {
+        public Command StartWorkflowCommand { get; private set; }
+
         public MainPageViewModel()
         {
             System.Diagnostics.Debug.WriteLine(DeviceInfo.Platform);
+
+            // Create a (currently) test workflow.
+            CreateWorkflow();
+
+            StartWorkflowCommand = new Command(obj =>
+            {
+                StartWorkflow();
+            });
+        }
+
+        void CreateWorkflow()
+        {
+
+        }
+
+        public void StartWorkflow()
+        {
+            System.Diagnostics.Debug.WriteLine("StartWorkflow");
         }
     }
 }
